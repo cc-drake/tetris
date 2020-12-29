f9776680a50c15aba0febceae948607ef7b37b16
cc-drake <sebastian.breiling@gmail.com>
2020-12-29 13:00:47 +0100

    InitialSpeed umbenannt, statische Default-Werte für Speed und Tastenbelegungen aufgenommen

--- src/de/drake/tetris/config/PlayerConfig.java
+++ src/de/drake/tetris/config/PlayerConfig.java
@@ -3,16 +3,46 @@ package de.drake.tetris.config;
 import java.util.Vector;
 
 import de.drake.tetris.input.util.InputManager;
+import de.drake.tetris.input.util.Key;
 
 public class PlayerConfig {
 	
 	public static Vector<PlayerConfig> playerConfigs;
+	
+	public static double initialSpeed = 2.;
+	
+	public static Key keyboard_left,
+			keyboard_right,
+			keyboard_down,
+			keyboard_drop,
+			keyboard_dreh_uzs,
+			keyboard_dreh_euzs,
+			keyboard_pause,
+			keyboard_quit;
+	
+	public static Key mouse_left,
+			mouse_right,
+			mouse_down,
+			mouse_drop,
+			mouse_dreh_uzs,
+			mouse_dreh_euzs,
+			mouse_pause,
+			mouse_quit;
+	
+	public static Key gamepad_left,
+			gamepad_right,
+			gamepad_down,
+			gamepad_drop,
+			gamepad_dreh_uzs,
+			gamepad_dreh_euzs,
+			gamepad_pause,
+			gamepad_quit;
 
 	private String name;
 	
 	private InputManager inputManager;
 	
-	private double initialSpeed;
+	private double speed;
 	
 	public PlayerConfig(final String name) {
 		this.name = name;
@@ -26,8 +56,8 @@ public class PlayerConfig {
 		return inputManager;
 	}
 
-	public double getInitialSpeed() {
-		return initialSpeed;
+	public double getSpeed() {
+		return speed;
 	}
 
 	public void setName(final String name) {
@@ -38,7 +68,7 @@ public class PlayerConfig {
 		this.inputManager = manager;
 	}
 
-	public void setInitialSpeed(final double speed) {
-		this.initialSpeed = speed;
+	public void setSpeed(final double speed) {
+		this.speed = speed;
 	}
 }
