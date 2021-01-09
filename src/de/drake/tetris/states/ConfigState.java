f53d90bbd40c36796949d7f621a5a83671576a19
cc-drake <cc-dr@Drake-PC>
2021-01-10 00:35:35 +0100

    Weitere ActionCommands zur Ini-Pflege

--- src/de/drake/tetris/states/ConfigState.java
+++ src/de/drake/tetris/states/ConfigState.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JComponent;
 
+import de.drake.tetris.config.IniInOut;
 import de.drake.tetris.screens.ConfigScreen;
 
 /**
@@ -16,6 +17,10 @@ public class ConfigState extends State implements ActionListener {
 	 * Der Screen, der im Display angezeigt wird.
 	 */
 	private final ConfigScreen screen;
+	
+	public final static String save = "In Tetris.ini speichern";
+	
+	public final static String reset = "Tetris.ini zurücksetzen";
 	
 	public final static String back = "Zurück";
 	
@@ -40,6 +45,23 @@ public class ConfigState extends State implements ActionListener {
 		if (e.getActionCommand() == ConfigState.back) {
 			if (this.screen.applyConfig())
 				State.setCurrentState(State.startState);
+			return;
+		}
+		if (e.getActionCommand() == ConfigState.save) {
+			if (this.screen.applyConfig()) {
+				try {
+					IniInOut.saveIni();
+				} catch (Exception e1) {
+				}
+			}
+			return;
+		}
+		if (e.getActionCommand() == ConfigState.reset) {
+			IniInOut.setConfig(null);
+			try {
+				IniInOut.saveIni();
+			} catch (Exception e2) {
+			}
 			return;
 		}
 	}
