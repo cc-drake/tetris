a6d69edbfeca166b5570f5e3af822195c1009c27
cc-drake <sebastian.breiling@gmail.com>
2021-06-03 12:24:18 +0200

    read braucht nicht initialisiert werden

--- src/de/drake/tetris/input/util/JInputLoader.java
+++ src/de/drake/tetris/input/util/JInputLoader.java
@@ -38,7 +38,7 @@ public class JInputLoader {
 		FileOutputStream os = new FileOutputStream(file);
 		
 		byte[] buffer = new byte[1024];
-		int read = -1;
+		int read;
 		while ((read = is.read(buffer)) != -1) {
 			os.write(buffer, 0, read);
 		}
